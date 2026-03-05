c97f2d5414b248915c424e0faf125369031f3466
Karanpaul85 <69508163+Karanpaul85@users.noreply.github.com>
2026-03-05 16:03:17 +0530

    add GTM code  (#12)
    * update state schema and create new components
    
    * make mobile responsive and sideBar
    
    * Create state schema
    
    * create district page according to new design
    
    * update district page according to new design
    
    * create new UI for tehsil page
    
    * update village schema and village sheet
    
    * create village page new UI
    
    * update limit related code in state and district
    
    * solve undefined issue for districts on state page
    
    * changes in API's for right sidbar data
    
    * finalized the village page
    
    * add weather in district page
    
    * add schema for district and village page
    
    * update site-map xml files
    
    * render right bar blocks if data exsit
    
    * update tehsil schema model
    
    * make sidebar sticky
    
    * add prettier code
    
    * add husky pulgin
    
    * update pre-commit file
    
    * update vsCode setting json
    
    * use common constant for REVALIDATE_TIME
    
    * revert REVALIDATE_TIME
    
    * add GTM code in main layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { DM_Sans, Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Script from "next/script";
 import { SessionProvider } from "next-auth/react";
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -13,20 +15,45 @@ const dmSans = DM_Sans({
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+      </head>
+
       <body
-        className={`${dmSans.className} antialiased min-h-screen flex flex-col`}
+        className={`${dmSans.className} flex min-h-screen flex-col antialiased`}
       >
-        <SessionProvider refetchInterval={60} refetchOnWindowFocus={true}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        <SessionProvider refetchInterval={60} refetchOnWindowFocus>
           <Header />
           {children}
           <Footer />
         </SessionProvider>
-        {/* Lordicon Web Component script */}
+
         <Script
           src="https://cdn.lordicon.com/lordicon.js"
           strategy="afterInteractive"
