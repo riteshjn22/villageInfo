ffac56c9329b1969a001f5ead852942947db426f
riteshjn22 <trendswe1@gmail.com>
2026-03-06 14:56:40 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
     <header className="bg-sky-950 text-white p-4 sticky top-0 z-50 shadow-indigo-200 shadow-md">
       <div className="flex w-full md:max-w-275 m-auto flex-wrap items-center justify-between">
         <Link href="/">
-          <h1 className="text-2xl font-bold">Village Info</h1>
+          <h1 className="text-2xl font-bold">Village Trends</h1>
         </Link>
         {isDashboard && session && (
           <button
