ec0fe5d2e3d01564b02fbff462648081f136f75c
riteshjn22 <trendswe1@gmail.com>
2026-03-09 08:56:18 +0530

    Update next.config.ts
    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,65 +12,127 @@ const nextConfig: NextConfig = {
   },
   async redirects() {
     return [
-      {
-        source: "/cleanest-village-in-india-mawlynnong",
-        destination:
-          "https://blog.trendswe.com/blog/cleanest-village-in-india-mawlynnong/",
-        permanent: true,
-      },
-      {
-        source: "/the-richest-village-of-maharashtra",
-        destination:
-          "https://blog.trendswe.com/blog/the-richest-village-of-maharashtra/",
-        permanent: true,
-      },
-      {
-        source: "/richest-village-in-india",
-        destination: "https://blog.trendswe.com/blog/richest-village-in-india/",
-        permanent: true,
-      },
-      {
-        source: "/top-10-richest-villages-in-india",
-        destination:
-          "https://blog.trendswe.com/blog/top-10-richest-villages-in-india/",
-        permanent: true,
-      },
-      {
-        source: "/haunted-village-in-rajasthan",
-        destination:
-          "https://blog.trendswe.com/blog/haunted-village-in-rajasthan/",
-        permanent: true,
-      },
-      {
-        source: "/black-magic-village-in-assam",
-        destination:
-          "https://blog.trendswe.com/blog/black-magic-village-in-assam/",
-        permanent: true,
-      },
-      {
-        source: "/1st-solar-village-in-india",
-        destination:
-          "https://blog.trendswe.com/blog/1st-solar-village-in-india/",
-        permanent: true,
-      },
-      {
-        source: "/sanskrit-speaking-village-in-india",
-        destination:
-          "https://blog.trendswe.com/blog/sanskrit-speaking-village-in-india/",
-        permanent: true,
-      },
-      {
-        source: "/largest-village-in-punjab",
-        destination:
-          "https://blog.trendswe.com/blog/largest-village-in-punjab/",
-        permanent: true,
-      },
-      {
-        source: "/largest-village-in-india",
-        destination: "https://blog.trendswe.com/blog/largest-village-in-india/",
-        permanent: true,
-      },
-    ];
+  {
+    "source": "/cleanest-village-in-india-mawlynnong",
+    "destination": "https://blog.trendswe.com/blog/cleanest-village-in-india-mawlynnong/",
+    "permanent": true
+  },
+  {
+    "source": "/the-richest-village-of-maharashtra",
+    "destination": "https://blog.trendswe.com/blog/the-richest-village-of-maharashtra/",
+    "permanent": true
+  },
+  {
+    "source": "/richest-village-in-india",
+    "destination": "https://blog.trendswe.com/blog/richest-village-in-india/",
+    "permanent": true
+  },
+  {
+    "source": "/top-10-richest-villages-in-india",
+    "destination": "https://blog.trendswe.com/blog/top-10-richest-villages-in-india/",
+    "permanent": true
+  },
+  {
+    "source": "/haunted-village-in-rajasthan",
+    "destination": "https://blog.trendswe.com/blog/haunted-village-in-rajasthan/",
+    "permanent": true
+  },
+  {
+    "source": "/black-magic-village-in-assam",
+    "destination": "https://blog.trendswe.com/blog/black-magic-village-in-assam/",
+    "permanent": true
+  },
+  {
+    "source": "/1st-solar-village-in-india",
+    "destination": "https://blog.trendswe.com/blog/1st-solar-village-in-india/",
+    "permanent": true
+  },
+  {
+    "source": "/sanskrit-speaking-village-in-india",
+    "destination": "https://blog.trendswe.com/blog/sanskrit-speaking-village-in-india/",
+    "permanent": true
+  },
+  {
+    "source": "/largest-village-in-punjab",
+    "destination": "https://blog.trendswe.com/blog/largest-village-in-punjab/",
+    "permanent": true
+  },
+  {
+    "source": "/largest-village-in-india",
+    "destination": "https://blog.trendswe.com/blog/largest-village-in-india/",
+    "permanent": true
+  },
+  {
+    "source": "/largest-village-in-nagaland",
+    "destination": "https://blog.trendswe.com/blog/largest-village-in-nagaland/",
+    "permanent": true
+  },
+  {
+    "source": "/biggest-village-in-haryana",
+    "destination": "https://blog.trendswe.com/blog/biggest-village-in-haryana/",
+    "permanent": true
+  },
+  {
+    "source": "/strongest-village-in-india",
+    "destination": "https://blog.trendswe.com/blog/strongest-village-in-india/",
+    "permanent": true
+  },
+  {
+    "source": "/smallest-village-in-india",
+    "destination": "https://blog.trendswe.com/blog/smallest-village-in-india/",
+    "permanent": true
+  },
+  {
+    "source": "/smallest-village-in-odisha",
+    "destination": "https://blog.trendswe.com/blog/smallest-village-in-odisha/",
+    "permanent": true
+  },
+  {
+    "source": "/twins-village-in-kerala",
+    "destination": "https://blog.trendswe.com/blog/twins-village-in-kerala/",
+    "permanent": true
+  },
+  {
+    "source": "/ias-village-in-up-india",
+    "destination": "https://blog.trendswe.com/blog/ias-village-in-up-india/",
+    "permanent": true
+  },
+  {
+    "source": "/ias-village-in-bihar",
+    "destination": "https://blog.trendswe.com/blog/ias-village-in-bihar/",
+    "permanent": true
+  },
+  {
+    "source": "/cyber-crime-village-in-india",
+    "destination": "https://blog.trendswe.com/blog/cyber-crime-village-in-india/",
+    "permanent": true
+  },
+  {
+    "source": "/smartest-village-in-india",
+    "destination": "https://blog.trendswe.com/blog/smartest-village-in-india/",
+    "permanent": true
+  },
+  {
+    "source": "/first-digital-village-in-andhra-pradesh",
+    "destination": "https://blog.trendswe.com/blog/first-digital-village-in-andhra-pradesh/",
+    "permanent": true
+  },
+  {
+    "source": "/india-last-village-in-south",
+    "destination": "https://blog.trendswe.com/blog/india-last-village-in-south/",
+    "permanent": true
+  },
+  {
+    "source": "/first-digital-village-in-india",
+    "destination": "https://blog.trendswe.com/blog/first-digital-village-in-india/",
+    "permanent": true
+  },
+  {
+    "source": "/first-bio-village-in-india",
+    "destination": "https://blog.trendswe.com/blog/first-bio-village-in-india/",
+    "permanent": true
+  }
+];
   },
 };
 
