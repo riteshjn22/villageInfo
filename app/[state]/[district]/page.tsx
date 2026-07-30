9fbe505cbdcaf7aee74e855e58429fa493cb0afa
riteshjn22 <trendswe1@gmail.com>
2026-07-31 00:05:10 +0530

    Update page.tsx

--- app/[state]/[district]/page.tsx
+++ app/[state]/[district]/page.tsx
@@ -21,7 +21,7 @@ import { cache } from "react";
 import { connectDB } from "@/lib/mongodb";
 import District from "@/lib/models/district";
 
-export const revalidate = 604801;
+export const revalidate = 604802;
 export const dynamicParams = true;
 
 // ─── Cached Fetchers ──────────────────────────────────────────────────────────
